ba4d0953dddf96539c23d8c440de7eb2e1c650ab
HABIBKAGHAS <habibkaghas@gmail.com>
2022-11-12 18:34:28 +0300

    upload to nft.storage !

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+            config.resolve.fallback = {
+                fs: false,
+            };
+        }
+
+        return config;
+    },
     reactStrictMode: false,
     swcMinify: true,
     env: {
         NEXT_PUBLIC_APP_ID: process.env.NEXT_PUBLIC_APP_ID,
         NEXT_PUBLIC_DAPP_URL: process.env.NEXT_PUBLIC_DAPP_URL,
         NEXT_PUBLIC_MASTER_KEY: process.env.NEXT_PUBLIC_MASTER_KEY,
+        NFT_STORAGE: process.env.NFT_STORAGE,
     },
 };
 
