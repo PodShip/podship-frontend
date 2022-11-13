afc33b8e6e39a5ee6b20f684c31bdde5a5c5e28d
HABIBKAGHAS <habibkaghas@gmail.com>
2022-11-13 15:50:16 +0300

    add little SEO

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,40 @@ import FourthPart from "../components/fourthPart-landingPage";
 import Footer from "../components/footer";
 
 export default function Home() {
-    // const { isWeb3Enabled, chainId } = useMoralis();
-    // const chainString = chainId ? parseInt(chainId).toString() : 31337;
-    // console.log({ chainId, chainString }, networkMapping[chainString]);
-    // const markeplaceAddress = networkMapping[chainString].NftMarketPlace;
-
     return (
         <>
+            <Head>
+                <title>Podship - Web3</title>
+                <link rel="shortcut icon" href="/logo.svg" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+                />
+                <meta name="keywords" content="podcast, nft, pods, web3, blockchain" />
+                <meta
+                    name="description"
+                    content="buy and sell your podcast on a web3 based platform"
+                />
+                <meta property="og:locale" content="en_EN" />
+                <meta property="og:site_name" content="Podship - Web3" />
+                <meta
+                    property="og:description"
+                    content="buy and sell your podcast on a web3 based platform"
+                />
+                <meta property="og:type" content="portfolio" />
+                <meta property="og:title" content="Podship - Web3" />
+                <meta property="og:image" content="https://podship.s3.amazonaws.com/podship.png" />
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:title" content="Podship - Web3" />
+                <meta
+                    property="twitter:description"
+                    content="buy and sell your podcast on a web3 based platform"
+                />
+                <meta
+                    property="twitter:image"
+                    content="https://podship.s3.amazonaws.com/podship.png"
+                />
+            </Head>
             <Header />
             <FirstPart />
             <SecondPart />
