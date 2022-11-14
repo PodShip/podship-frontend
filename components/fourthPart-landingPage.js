df97343987962581b33be8e10070649b51c807a3
HABIBKAGHAS <habibkaghas@gmail.com>
2022-11-14 10:33:30 +0300

    edit powered by part

--- components/fourthPart-landingPage.js
+++ components/fourthPart-landingPage.js
@@ -1,41 +1,32 @@
 export default function FourthPart() {
     return (
         <div
-            className="bg-no-repeat bg-center h-screen w-screen p-14 items-center"
+            className="bg-no-repeat bg-center bg-cover h-screen w-screen  items-center"
             style={{ backgroundImage: "url(fourthPartImage.png)" }}
         >
-            <div className="grid grid-flow-col auto-cols-3 items-center">
+            <div className="grid grid-flow-col grid-cols-3 items-center  w-screen pt-64">
                 <div></div>
-                <div>
-                    <h1 className="fourthPart-Primary items-center pt-52 pl-64">
-                        Never miss a podcast
-                    </h1>
+                <div className="col-span-2 items-center">
+                    <h1 className="powerdBy items-center ml-24">Powered By</h1>
                 </div>
                 <div></div>
             </div>
-            <div className="grid grid-flow-col auto-cols-3 items-center">
-                <div></div>
-                <div>
-                    <p className="pl-64">
-                        Subscribe to our newsletter to get the news podcast drops
-                    </p>
+            <div className="grid grid-flow-col grid-cols-5 items-center w-screen m-24">
+                <div className="items-center">
+                    <img src="powerdByLogos/QuickNode.svg" />
                 </div>
-                <div></div>
-            </div>
-            <div className="grid grid-flow-col auto-cols-3 items-center">
-                <div></div>
-                <div>
-                    <div className="pl-64 pt-14">
-                        <input
-                            type={"email"}
-                            className="fourthPart-input m-1 p-2 bg-transparent items-center"
-                            placeholder="Name.Surname@mail.com"
-                            size={50}
-                        ></input>
-                        <a className="primary-btn pl-10 pr-10 pt-5 pb-5 ml-10">Submit</a>
-                    </div>
+                <div className="items-center">
+                    <img src="powerdByLogos/polygon.svg" />
                 </div>
-                <div></div>
+                <div className="items-center">
+                    <img src="powerdByLogos/Chainlink.svg" />
+                </div>
+                <div className="items-center">
+                    <img src="powerdByLogos/IPFS.svg" />
+                </div>
+                <div className="items-center">
+                    <img src="powerdByLogos/filecoin.svg" />
+                </div>
             </div>
         </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,9 @@ export default function Home() {
             <FirstPart />
             <SecondPart />
             <ThirdPart />
+            <br />
             <FourthPart />
+            <br />
             <Footer />
         </>
     );
